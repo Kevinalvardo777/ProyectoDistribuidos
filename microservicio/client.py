b58f6c5d179bdc10ff98286856a30887388de88d
bryandario8 <bryandario1994@gmail.com>
2018-08-23 04:26:48 +0000

    respaldo

--- microservicio/client.py
+++ microservicio/client.py
@@ -12,7 +12,7 @@ from thrift.protocol import TBinaryProtocol
 
 def main():
     # Make socket
-    transport = TSocket.TSocket('localhost', 9090)
+    transport = TSocket.TSocket('localhost', 22)
 
     # Buffering is critical. Raw sockets are very slow
     transport = TTransport.TBufferedTransport(transport)
